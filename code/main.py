c56a5ebd104b433e977c751cf1f6dc603df36fc0
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-04-02 16:54:04 +0530

    crorrecting json load

--- code/main.py
+++ code/main.py
@@ -1,12 +1,18 @@
 
 import os
 import json
-
+from json import JSONDecodeError
 from azureml.core import Workspace
 from azureml.core.authentication import ServicePrincipalAuthentication
 
 from azureml.core import Experiment
 from fetchmetric import getMetrics
+
+
+class AMLConfigurationException(Exception):
+    pass
+
+
 def get_spAuth(azure_credentials):
     sp_auth = ServicePrincipalAuthentication(
             tenant_id=azure_credentials.get("tenantId", ""),
@@ -18,7 +24,15 @@ def get_spAuth(azure_credentials):
 def main():
     azure_credentials = os.environ.get("INPUT_AZURECREDENTIALS", default='{}')
     azureml_workSpaceName = os.environ.get("INPUT_WORKSPACENAME", default=None)
+    experiment_name = os.environ.get("INPUT_EXPERIMENTNAME", default=None)
     github_SHA = os.environ.get("INPUT_COMMIT_SHA", default="7a6fe10d22b5c44be55698f6d123c6480451e18b")
+
+    try:
+        azure_credentials = json.loads(azure_credentials)
+    except JSONDecodeError:
+        print("::error::Please paste output of `az ad sp create-for-rbac --name <your-sp-name> --role contributor --scopes /subscriptions/<your-subscriptionId>/resourceGroups/<your-rg> --sdk-auth` as value of secret variable: AZURE_CREDENTIALS. The JSON should include the following keys: 'tenantId', 'clientId', 'clientSecret' and 'subscriptionId'.")
+        raise AMLConfigurationException(f"Incorrect or poorly formed output from azure credentials saved in AZURE_CREDENTIALS secret. See setup in https://github.com/Azure/aml-workspace/blob/master/README.md")
+        
 
     if azureml_workSpaceName == None or azure_credentials == {}:
         print(" credentials empty or worksapce name can not be empty")
